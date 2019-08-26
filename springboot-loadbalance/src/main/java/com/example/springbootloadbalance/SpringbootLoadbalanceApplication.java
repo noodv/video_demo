50c73774a70fd26f7126c6c25d5a7f1c1549cd23
diesel <yuediesel@gmail.com>
2019-08-27 01:11:50 +0800

    tf

---
+++ springboot-loadbalance/src/main/java/com/example/springbootloadbalance/SpringbootLoadbalanceApplication.java
@@ -0,0 +1,13 @@
+package com.example.springbootloadbalance;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootLoadbalanceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootLoadbalanceApplication.class, args);
+    }
+
+}
