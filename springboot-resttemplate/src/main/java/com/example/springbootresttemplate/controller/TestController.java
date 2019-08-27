8841712913fff81eafb5ad48c44aa053ee19ffb7
diesel <yuediesel@gmail.com>
2019-08-27 15:24:26 +0800

    ffd

---
+++ springboot-resttemplate/src/main/java/com/example/springbootresttemplate/controller/TestController.java
@@ -0,0 +1,25 @@
+package com.example.springbootresttemplate.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+@RestController
+public class TestController {
+
+    @Autowired
+    private RestTemplate restTemplate;
+
+    @RequestMapping("test1")
+    public void test1() {
+        String response = restTemplate.getForObject("http://RESTTEMPLATE/test2",String.class);
+        System.out.println(response);
+    }
+
+    @RequestMapping("test2")
+    public String test2() {
+        return "测试数据： test2";
+    }
+
+}
