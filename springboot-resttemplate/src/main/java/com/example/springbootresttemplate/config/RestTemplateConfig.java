c0a2956851464ef21e8c1c6eac5c0475670e2d5a
diesel <yuediesel@gmail.com>
2019-08-27 16:15:25 +0800

    fdfds

--- springboot-resttemplate/src/main/java/com/example/springbootresttemplate/config/RestTemplateConfig.java
+++ springboot-resttemplate/src/main/java/com/example/springbootresttemplate/config/RestTemplateConfig.java
@@ -3,6 +3,7 @@ package com.example.springbootresttemplate.config;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.client.SimpleClientHttpRequestFactory;
 import org.springframework.web.client.RestTemplate;
 
 @Configuration
@@ -10,8 +11,13 @@ public class RestTemplateConfig {
 
     @Bean
     @LoadBalanced
-    public RestTemplate restTemplate() {
-        return new RestTemplate();
+    public RestTemplate restTemplate()
+    {
+        SimpleClientHttpRequestFactory requestFactory = new SimpleClientHttpRequestFactory();
+        requestFactory.setConnectTimeout(5000);
+        requestFactory.setReadTimeout(3000);
+        RestTemplate restTemplate = new RestTemplate(requestFactory);
+        return restTemplate;
     }
 
 }
