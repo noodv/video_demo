ea6ef9e8f0eb4f6e191c3fdcc5da34012320d076
diesel <yuediesel@gmail.com>
2019-08-28 14:42:23 +0800

    fdsaf

--- springboot-feiqn/src/main/java/com/example/springbootresttemplate/service/SchedualServiceHi.java
+++ springboot-feiqn/src/main/java/com/example/springbootresttemplate/service/SchedualServiceHi.java
@@ -5,7 +5,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 
-@FeignClient(name = "service-feign")   // 注意，这里要指定已有的服务名称,还有注意name和value的区别
+@FeignClient(name = "service-feign")   // 注意，这里要指定已有的服务名称,还有注意name和value的区别,
 public interface SchedualServiceHi {
 
     @RequestMapping(value = "test2", method = RequestMethod.GET)
